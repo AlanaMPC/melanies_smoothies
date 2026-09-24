ca1772c2575ae8eae02a008710f1001a6c30f4b2
AlanaMPC <alanamariyapc@gmail.com>
2026-09-24 16:48:54 +0530

    Display Smoothiefroot nutrition information
    Added functionality to display Smoothiefroot nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,11 +38,15 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
 # New section to display smoothiefroot nutrition information
-import requests
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")    
+   
     
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+import requests
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
+# Display JSON text
+st.text(smoothiefroot_response.json())
