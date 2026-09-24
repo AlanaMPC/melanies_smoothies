120c2dea3c1ae34304ff7853af9f90c13f16ff41
AlanaMPC <alanamariyapc@gmail.com>
2026-09-24 16:50:27 +0530

    Display Smoothiefroot response as a Streamlit dataframe
    Replaced text display of JSON response with a Streamlit dataframe for better visualization.

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,5 @@ import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
 # Display JSON text
-st.text(smoothiefroot_response.json())
+# Convert JSON directly into a Streamlit dataframe
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
